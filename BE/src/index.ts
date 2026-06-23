2a70874fc71149eee5861166a056b97641affcb7
Thisitha-Liyanage <thisithaliyanage@gmail.com>
2026-06-23 12:47:22 +0530

    pakage.json update

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -8,10 +8,10 @@ import noteRoutes from "./Routers/NoteRoute";
 import messageRoutes from "./Routers/MessageRoute";
 import adminRoutes from "./Routers/AdminRoutes";
 
-
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
+
 app.use(
   cors({
     origin: [
@@ -23,29 +23,29 @@ app.use(
 );
 
 app.use(express.json());
+
 app.use("/api/users", userRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/admin", adminRoutes);
 
+app.get("/", (req, res) => {
+  res.send("StudyLink Backend Running");
+});
+
 // DB CONNECT
 mongoose
   .connect(process.env.DB_URL as string)
   .then(() => {
     console.log("MongoDB Connected");
-
-    // start server ONLY after DB connects
-    const PORT = process.env.PORT || 5000;
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((err) => {
     console.error("MongoDB connection failed:", err);
   });
 
-app.get("/", (req, res) => {
-  res.send("StudyLink Backend Running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
