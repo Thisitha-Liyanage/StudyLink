b3eee5457412723f6bacc36ad15858a2c915f2f3
Thisitha-Liyanage <thisithaliyanage@gmail.com>
2026-06-25 09:15:35 +0530

    fix: update CORS configuration to allow all origins

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow all origins for now
+    origin: "*", // Allow all origins for now
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
